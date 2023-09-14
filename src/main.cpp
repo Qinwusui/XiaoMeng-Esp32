a833729cf8076bfca40d8299f239f5ef75f1f2a3
qinwusui <2064508450@qq.com>
2023-09-14 19:28:10 +0800

    firstCommit

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include <main.h>
+//变量定义区
+TaskHandle_t connectWifiTask = NULL;
+TaskHandle_t createWiFiServer = NULL;
+AsyncWebServer server(80);
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    Serial.flush();
+    Serial.flush();
+    Serial.println("WelCome");
+    if (!LittleFS.begin(true)) {
+        Serial.println("打开SPIFFS失败");
+        return;
+    }
+    pinMode(21 , OUTPUT);
+    pinMode(19 , OUTPUT);
+    Serial.println("创建任务中...");
+    if (xTaskCreate(
+        vTaskConnectWifi ,
+        "ConnectWifi" ,
+        10240 ,
+        NULL ,
+        10 ,
+        &connectWifiTask
+    ) != pdPASS) {
+        vTaskDelete(connectWifiTask);
+        Serial.println("创建连接WiFi任务失败");
+    }
+
+    // if (xTaskCreate(
+    //     vTaskCreateWiFiServer ,
+    //     "createWiFiServer" ,
+    //     10240 ,
+    //     NULL ,
+    //     1 ,
+    //     &createWiFiServer
+    // ) != pdPASS) {
+    //     vTaskDelete(createWiFiServer);
+    //     Serial.println("创建web服务器任务失败");
+    // }
+
+}
+
+void loop() {}
+
+void vTaskConnectWifi(void* param) {
+    WifiConfig* config = getWifiConfig();
+    if (config == NULL) {
+        xTaskCreate(
+            vTaskCreateWiFiServer ,
+            "createWiFiServer" ,
+            10240 ,
+            NULL ,
+            1 ,
+            &createWiFiServer
+        );
+        vTaskDelete(connectWifiTask);
+    }
+    WiFi.begin(config->SSID , config->PWD);
+    Serial.printf("正在连接到%s" , config->SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    Serial.println();
+    Serial.printf("已连接到%s\n" , config->SSID);
+    Serial.flush();
+    vTaskDelete(connectWifiTask);
+}
+WifiConfig* getWifiConfig() {
+    File f = LittleFS.open(wifiPath , "r");
+    if (!f) {
+        Serial.println("获取WiFi配置失败，将启动热点并开启服务器进行配网操作");
+        f.close();
+        return NULL;
+    }
+    String content = f.readString();
+
+    WifiConfig* config = new WifiConfig();
+    int index = content.indexOf("\\||");
+    String ssid = content.substring(0 , index);
+    String pwd = content.substring(index + 1 , content.length());
+    Serial.printf("获取WiFi配置成功%s:%s\n" , ssid , pwd);
+    config->SSID = ssid;
+    config->PWD = pwd;
+    return config;
+}
+void vTaskCreateWiFiServer(void* p) {
+    WiFi.mode(WIFI_MODE_APSTA);
+
+    IPAddress localIp = IPAddress(192 , 168 , 1 , 1);
+    IPAddress gateWay = IPAddress(192 , 168 , 1 , 1);
+    IPAddress subNet = IPAddress(255 , 255 , 255 , 0);
+    WiFi.softAPConfig(localIp , gateWay , subNet);
+    WiFi.softAP("me" , "qinsansui233" , 12 , 0 , 16);
+
+    server.serveStatic("/" , LittleFS , "/web/").setDefaultFile("index.html");
+
+    server.begin();
+    vTaskDelete(NULL);
+}
---
+++ src/main.h
@@ -0,0 +1,23 @@
+//依赖导入区
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include <LittleFS.h>
+
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+//常量配置区
+const String wifiPath = "wifi.conf";
+
+//函数定义区
+void vTaskConnectWifi(void* param);
+
+//对象定义区
+struct WifiConfig {
+    String SSID;
+    String PWD;
+};
+WifiConfig* getWifiConfig();
+
+void vTaskCreateWiFiServer(void* p);
